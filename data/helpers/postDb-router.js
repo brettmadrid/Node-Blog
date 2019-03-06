647390faa6deeca66a2368c4855c274a2cb3800f
brettmadrid <brettmadrid9@gmail.com>
2019-03-06 00:40:44 -0800

    Post now has routers set up for all CRUD

--- data/helpers/postDb-router.js
+++ data/helpers/postDb-router.js
@@ -1,85 +1,76 @@
-const express = require('express');
+const express = require("express");
 
-const Posts = require('./postDb.js');
+const Posts = require("./postDb.js");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+// GET COMPLETE
+router.get("/", async (req, res) => {
   try {
     const posts = await Posts.get(req.query);
     res.status(200).json(posts);
   } catch (error) {
-    // log error to database
-    console.log(error);
     res.status(500).json({
-      message: 'Error retrieving the posts',
+      message: "Error retrieving the posts"
     });
   }
 });
 
-//POST
-router.post('/', async (req, res) => {
+//POST COMPLETE
+router.post("/", async (req, res) => {
   try {
     const newPost = await Posts.insert(req.body);
     res.status(201).json(newPost);
-  }
-  catch (error) {
-    console.log(error);
+  } catch (error) {
     res.status(500).json({
       message: "Error adding the post."
-    }); 
-  }
-});
-
-// COMPLETE
-router.delete('/:id', async (req, res) => {
-  try {
-    const count = await Posts.remove(req.params.id);
-  
-    if (count > 0) {
-      res.status(200).json({ message: 'The post has been nuked' });
-    } else {
-      res.status(404).json({ message: 'The id could not be found' });
-    }
-  } catch (error) {
-    // log error to database
-    console.log(error);
-    res.status(500).json({
-      message: 'Error removing the user',
     });
   }
 });
 
-router.put('/:id', async (req, res) => {
-  const post = req.body;
+// DELETE COMPLETE
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Posts.remove(req.params.id);
 
-  if (post) {
-    try {
-      const response = await Posts.update(req.params.id, post);
-      response === 1 ? res.status(200).json(post) : res.status(404).json({message: "Post does not exist"});
-    } catch(error) {
-      res.status(500).json({message: "Post info could not be modified"});
+    if (post) {
+      res.status(200).json({ message: "The post has been deleted" });
+    } else {
+      res.status(404).json({ message: "The id could not be found" });
     }
-
-  } else {
-    res.status(400).json({message: "please enter valid post"});
+  } catch (error) {
+    res.status(500).json({
+      message: "Error removing the user"
+    });
   }
 });
 
-// COMPLETE
-router.get('/:id', async (req, res) => {
+// UPDATE COMPLETE
+router.put("/:id", async (req, res) => {
+    try {
+      const updated = await Posts.update(req.params.id, req.body);
+      updated
+        ? res.status(200).json(updated)
+        : res.status(404).json({ message: "The post cannot be found!" });
+    } catch (error) {
+      res.status(500).json({ message: "Post info could not be modified" });
+    }
+  
+});
+
+// GET BY ID COMPLETE
+router.get("/:id", async (req, res) => {
   try {
     const post = await Posts.getById(req.params.id);
 
-    if(post) {
+    if (post) {
       res.status(200).json(post);
     } else {
-      res.status(404).json({message: 'Post not found'});
+      res.status(404).json({ message: "Post not found" });
     }
-  } catch(error) {
-    res.status(500).json({error: "The post could not be retrieved"});
+  } catch (error) {
+    res.status(500).json({ error: "The post could not be retrieved" });
   }
 });
-
 
 module.exports = router;
--- data/helpers/userDb-router.js
+++ data/helpers/userDb-router.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 
-const Users = require('./userDb.js');
+const Users = require("./userDb.js");
 
 const router = express.Router();
 
@@ -11,26 +11,23 @@ function allCaps(req, res, next) {
 }
 
 // GET
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const users = await Users.get(req.query);
     res.status(200).json(users);
   } catch (error) {
-    // log error to database
-    console.log(error);
     res.status(500).json({
-      message: 'Error retrieving users',
+      message: "Error retrieving users"
     });
   }
 });
 
 // POST COMPLETE
-router.post('/', allCaps, async (req, res) => {
+router.post("/", allCaps, async (req, res) => {
   try {
     const newUser = await Users.insert(req.body);
     res.status(201).json(newUser);
   } catch (error) {
-    console.log(error);
     res.status(500).json({
       message: "Error adding the user."
     });
@@ -38,66 +35,62 @@ router.post('/', allCaps, async (req, res) => {
 });
 
 // GET BY ID COMPLETE
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const user = await Users.getById(req.params.id);
 
-    if(user) {
+    if (user) {
       res.status(200).json(user);
     } else {
-      res.status(404).json({message: 'User not found'});
+      res.status(404).json({ message: "User not found" });
     }
-  } catch(error) {
-    res.status(500).json({error: "The user could not be retrieved"});
+  } catch (error) {
+    res.status(500).json({ error: "The user could not be retrieved" });
   }
 });
 
 // DELETE COMPLETE
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const user = await Users.remove(req.params.id);
 
     if (user) {
       res.status(200).json(user);
     } else {
-      res.status(404).json({ message: 'The id could not be found' });
+      res.status(404).json({ message: "The id could not be found" });
     }
   } catch (error) {
     res.status(500).json({
-      message: 'Error removing the user',
+      message: "Error removing the user"
     });
   }
 });
 
 // UPDATE COMPLETE
-router.put('/:id', allCaps, async (req, res) => {
+router.put("/:id", allCaps, async (req, res) => {
   try {
     const updatedUser = await Users.update(req.params.id, req.body);
     if (updatedUser) {
       res.status(200).json(updatedUser);
     } else {
-      res.status(404).json({ message: 'The User could not be found' });
+      res.status(404).json({ message: "The User could not be found" });
     }
   } catch (error) {
-    // log error to database
-    console.log(error);
     res.status(500).json({
-      message: 'Error updating the user',
+      message: "Error updating the user"
     });
   }
 });
 
-router.get('/:id/posts', async (req, res) => {
+router.get("/:id/posts", async (req, res) => {
   try {
     const posts = await Users.getUserPosts(req.params.id);
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({
-      message: 'Error getting the posts for the user',
+      message: "Error getting the posts for the user"
     });
   }
 });
-
-
 
 module.exports = router;
